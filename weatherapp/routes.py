4904b87f580a1b9c4bd6eb97fb45509e96d52046
Karol Tabaka <karol-tabaka@wp.pl>
2022-12-03 08:58:01 +0100

    New html files layout.

--- weatherapp/routes.py
+++ weatherapp/routes.py
@@ -3,11 +3,11 @@ from flask import request
 from weatherapp import app, api_queries
 
 @app.route('/', methods=['GET', 'POST'])
-def index():
+def home():
     if request.method == 'POST':        
         weather = api_queries.get_weather(request.form['city_name'])
         if weather:
-            return render_template('index.html', weather=weather)
-        return render_template('index.html', weather='Not Found')
+            return render_template('home.html', weather=weather)
+        return render_template('home.html', weather='Not Found')
     elif request.method == 'GET':
-        return render_template('index.html')
+        return render_template('home.html')
